40137c199c87c7468ada5a66060c92764ed92386
Moses Madavaram <mosesgab@gmail.com>
2023-05-30 13:29:47 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from Weather_Data import get_data
+from Display_Data import result_data
 
+def main():
+    #Here we call the different files and the methods involved with it.
+    Weather_Data = get_data()
+    Display_Data = result_data(Weather_Data)
+
+# Here we call the main function.
+if __name__ == "__main__":
+    main()
